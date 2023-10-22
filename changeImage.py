4e676dc0f528f0bbba4c54baab8f5012fbd091cc
alican <alican93metu@gmail.com>
2023-10-22 15:53:13 +0300

    First stable version of changeImage.py

---
+++ changeImage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from PIL import Image
+import os
+
+location = "supplier-data/images"
+for infile in os.listdir(location):
+    if infile.endswith(".tiff"):
+        file, ext = os.path.splitext(infile)
+        with Image.open(location + "/" + infile) as im:
+            im.resize((600,400)).convert("RGB").save(location + "/" + file + ".jpeg")
+        os.remove(location + "/" + infile)
